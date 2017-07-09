f86317d215899aafc3f37d089a0d231cc663f177
roman <roman@romandeMacBook-Pro.local>
2017-07-09 21:54:49 +0800

    add

--- src/index.js
+++ src/index.js
@@ -3,12 +3,26 @@
 
 const sweetScroll = new SweetScroll();
 $(document).on("click", ".js-back-to-top", function(e) {
-    e.preventDefault(),
-    e.stopPropagation(),
+    e.preventDefault();
+    e.stopPropagation();
     sweetScroll.toTop(0)
 });
 $(document).on("click", ".icon-reorder", function(e) {
-    e.preventDefault(),
-    e.stopPropagation(),
+    e.preventDefault();
+    e.stopPropagation();
     $('body').addClass('fix_body');
+});
+$(document).on("click", ".c-site-menu-toggle__inner", function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $('body').removeClass('fix_body');
+});
+$(document).on("click", "#content .category", function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    if($(this).parent().hasClass('active')){
+        $(this).parent().removeClass('active')
+    }else{
+        $(this).parent().addClass('active');
+    }
 });
